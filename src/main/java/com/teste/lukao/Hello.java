0016d32e50f569be9e6dee49dc0831e791459911
Lucas Moreira <lucasmoreira.aes@gmail.com>
2019-08-29 11:37:30 -0300

    :pencil2: Quarto Commit

--- src/main/java/com/teste/lukao/Hello.java
+++ src/main/java/com/teste/lukao/Hello.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping
     public String hello(){
-        return "Olá, tudo bem? Agora tem um deploy!";
+        return "Olá, tudo bem? Agora tem um deploy automatico...";
     }
 }
