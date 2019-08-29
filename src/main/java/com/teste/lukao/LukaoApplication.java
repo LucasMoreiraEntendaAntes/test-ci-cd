a2396cad8cf2b412284df3588dcdee53f39ed4d7
Lucas Moreira <lucasmoreira.aes@gmail.com>
2019-08-29 10:28:38 -0300

    :tada: Primeiro Commit

---
+++ src/main/java/com/teste/lukao/LukaoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.lukao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukaoApplication.class, args);
+	}
+
+}
