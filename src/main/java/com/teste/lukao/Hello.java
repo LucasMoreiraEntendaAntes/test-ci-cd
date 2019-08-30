e66b9f5a9c3dacbf6d5faaa9278abac5a3c52d39
Lucas Moreira <lucasmoreira.aes@gmail.com>
2019-08-30 09:27:22 -0300

    :pencil2: Oitavo Commit

--- src/main/java/com/teste/lukao/Hello.java
+++ src/main/java/com/teste/lukao/Hello.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping
     public String hello(){
-        return "Olá, tudo bem? Deploy rocks, baby!!!";
+        return "Olá, tudo bem? Deploy de sucesso, let's rock!";
     }
 }
