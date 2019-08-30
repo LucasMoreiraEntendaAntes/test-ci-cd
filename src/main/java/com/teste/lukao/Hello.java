cecd8e8d090ba01b59758963668304253d537921
Lucas Moreira <lucasmoreira.aes@gmail.com>
2019-08-30 10:19:03 -0300

    :pencil2: Decimo Primeiro Commit

--- src/main/java/com/teste/lukao/Hello.java
+++ src/main/java/com/teste/lukao/Hello.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping
     public String hello(){
-        return "Olá, tudo bem? Deploy de sucesso, rock with it!";
+        return "Olá, tudo bem? Deploy de sucesso, rock n' roll!";
     }
 }
